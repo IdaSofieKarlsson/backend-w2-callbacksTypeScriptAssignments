a8c550440a0b6bef61063169f4256dbee3aef0a5
Ida-Sofie Karlsson <ida.sofie.karlsson@gmail.com>
2025-09-02 11:06:45 +0200

    exercise 9 done

--- src/a09-math-operations.ts
+++ src/a09-math-operations.ts
@@ -1,2 +1,34 @@
 //Write one function that can do addition, subtraction, multiplication, and division. 
 // It should take two numbers, an operation string, and a callback.
+
+type operationFunction = (a: number, b: number, operatorChoice: string) => number | undefined;
+
+//main function takes in arguments, takes in result from callback function and logs the result
+const fourMathAlternatives = (a: number, b: number, operatorChoice: string, operator: operationFunction) => {
+    let result: number | undefined = operator(a, b, operatorChoice);
+    console.log(result);
+};
+
+//the callback function takes choice of math operator as argument, peforms the calculation and returns the result. 
+//the use of return means that break is not needed
+const operator = (a: number, b: number, operatorChoice: string) => {
+    switch (operatorChoice) {
+        case 'addition':
+            return a + b;
+        case 'subtraction':
+            return a - b;
+        case 'multiplication':
+            return a * b;
+        case 'division': 
+            return a / b;
+        default: 
+            console.log('error');
+            break;
+    };
+};
+
+//calling the main function
+fourMathAlternatives(4, 5, 'addition', operator);
+fourMathAlternatives(4, 5, 'subtraction', operator);
+fourMathAlternatives(4, 5, 'multiplication', operator);
+fourMathAlternatives(4, 5, 'division', operator);
--- src/testbed.ts
+++ src/testbed.ts
@@ -14,3 +14,5 @@ const displayData = (data: string): void => {
 };
 
 deliverPackage(displayData);
+
+ 
