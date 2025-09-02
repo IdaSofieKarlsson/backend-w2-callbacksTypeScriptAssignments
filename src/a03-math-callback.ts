38fdc88ae75873659b7a15cc23c85c6b926da919
Ida-Sofie Karlsson <ida.sofie.karlsson@gmail.com>
2025-09-02 02:18:45 +0200

    exercise 3 done

--- src/a03-math-callback.ts
+++ src/a03-math-callback.ts
@@ -1,1 +1,13 @@
 //Create a function that takes two numbers and a callback. The function should add the numbers and send the result to the callback.
+type printResultFunction = (result: number) => void;
+
+const addNumbers = (a: number, b: number, printResult: printResultFunction) => {
+    const result = a + b;
+    printResult(result);
+};
+
+const printResult = (result: number) => {
+    console.log(`The result is: ${result}`);
+};
+
+addNumbers(47, 53, printResult);
