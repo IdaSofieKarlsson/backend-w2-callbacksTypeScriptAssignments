9f24279ed6ba5acc407ead7c31e7c5615b8d980b
Ida-Sofie Karlsson <ida.sofie.karlsson@gmail.com>
2025-09-02 03:10:35 +0200

    exercise 04 done

--- src/a04-uppercase-callback.ts
+++ src/a04-uppercase-callback.ts
@@ -1,1 +1,14 @@
 //Write a function that takes a string and a callback. The callback should return the string in uppercase.
+
+type convertToUppercaseFunction = (myString: string) => string;
+
+const convertToUppercase = (myString: string, convertStringToUppercase: convertToUppercaseFunction) => {
+    const upperString: string = convertStringToUppercase(myString);
+    console.log(upperString);
+};
+
+const convertStringToUppercase = (myString: string): string => {
+    return myString.toUpperCase();
+};
+
+convertToUppercase('we started as lowercase' , convertStringToUppercase);
