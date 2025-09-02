0460f6b72fb6f6c8691fa4b7cd13f0d0a227c93a
Ida-Sofie Karlsson <ida.sofie.karlsson@gmail.com>
2025-09-02 03:49:32 +0200

    exercise 8 done

--- src/a08-success-error.ts
+++ src/a08-success-error.ts
@@ -1,1 +1,26 @@
 //Make a function that takes two callbacks: one for success and one for error. Use Math.random() to decide which to call.
+
+
+const randomMessage = () => {
+    const randomNumber: number = Math.floor(Math.random() * 2);
+    if (randomNumber === 0) {
+        successMessage();
+    } else if (randomNumber === 1) {
+        errorMessage();
+    } else {
+        console.log("The function didn't work.");
+    };
+};
+
+//the callback functions does not have parametres, as they only print a message
+//callback function for success message
+const successMessage = () => {
+    console.log('We have succeded, yet again!!!!!!!!!!!!!');
+};
+
+//callback function for error message
+const errorMessage = () => {
+    console.log("Well, THAT didn't go well... massive errors here.");
+};
+
+randomMessage();
