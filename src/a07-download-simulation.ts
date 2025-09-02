cd3c2aeae3981174de4bbb5bf13f40196cf15f4b
Ida-Sofie Karlsson <ida.sofie.karlsson@gmail.com>
2025-09-02 02:41:13 +0200

    exercise 7 done

--- src/a07-download-simulation.ts
+++ src/a07-download-simulation.ts
@@ -1,1 +1,15 @@
 //Create a function that takes a URL string and a callback. Wait 2 seconds, then call the callback with 'Downloaded data from <URL>'.
+type showFileFunction = (fileContent: string) => void;
+
+const downloadFile = (url: string, showFile: showFileFunction) => {
+    setTimeout(() => {
+        const fileContent = `Downloaded data from ${url}`;
+        showFile(fileContent);
+    }, 2000);
+};
+
+const showFile = (fileContent: string) => {
+    console.log(fileContent);
+};
+
+downloadFile("http://exemple.com/file.txt", showFile);
